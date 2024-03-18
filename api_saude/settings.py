44c668bec64f016ae3009452935bf853d6788a7f
Wesley <brunoxbk@gmail.com>
2024-03-18 15:37:47 -0300

    ajuste render

--- api_saude/settings.py
+++ api_saude/settings.py
@@ -115,24 +115,24 @@ WSGI_APPLICATION = "api_saude.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if not ON_RENDER:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.%s" % os.environ.get("DB_ENGINE"),
-            "NAME": os.environ.get("DB_NAME"),
-            "USER": os.environ.get("DB_USER"),
-            "PASSWORD": os.environ.get("DB_PASSWORD"),
-            "HOST": os.environ.get("DB_HOST"),
-            "PORT": os.environ.get("DB_PORT"),
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get("DB_URL"),
-            conn_max_age=600
-        )
-    }
+# if not ON_RENDER:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.%s" % os.environ.get("DB_ENGINE"),
+#             "NAME": os.environ.get("DB_NAME"),
+#             "USER": os.environ.get("DB_USER"),
+#             "PASSWORD": os.environ.get("DB_PASSWORD"),
+#             "HOST": os.environ.get("DB_HOST"),
+#             "PORT": os.environ.get("DB_PORT"),
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DB_URL"),
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
